2f3820330560e25bed16f7b57a40b2d0874091b8
Andrzej <andrzej@rpi.pl>
2023-02-15 17:30:30 +0000

    fog

--- src/PixelShader.hlsl
+++ src/PixelShader.hlsl
@@ -8,7 +8,10 @@ Texture2D texture_ps;
 SamplerState sampler_ps;
 
 float4 main(ps_input_t input) : SV_TARGET {	
+	float fogy = clamp(10.2 - input.position.z * 10.3 , 0, 1);
+	float4 fog_color = float4(0.0f, 0.8f, 0.8f, 1.0f);
+
 	return input.color
 	       * texture_ps.Sample(sampler_ps, input.tex)
-		   * clamp(1.2 - input.position.z , 0, 1);
+		   * fogy + (1 - fogy) * fog_color;
 }
