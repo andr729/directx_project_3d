97bbea931a314d06febf88ff35c663b66737bb2f
Andrzej <andrzej@rpi.pl>
2023-02-16 07:57:20 +0000

    ambient light reduction

--- src/VertexShader.hlsl
+++ src/VertexShader.hlsl
@@ -29,7 +29,7 @@ vs_output_t main(
 	
 	result.position = mul(mul(float4(pos, 1.0f), mat_w), matWorldViewProj);
 	result.color = mul(
- 			max(-dot(normalize(LW), normalize(NW)), 0.4f), 
+ 			max(-dot(normalize(LW), normalize(NW)), 0.2f), 
  			colLight * col);
 
 	result.tex = tex;
